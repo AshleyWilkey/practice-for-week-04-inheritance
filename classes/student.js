8a7701705097cda1ecb61b242d2ea0b08f23498c
Ashley Wilkey <contact@ashleywilkey.com>
2024-11-18 22:10:56 -0800

    inheritance practice

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,22 @@
-const Person = require('./person');
+const Person = require("./person");
 
 // Your code here
+
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(student1, student2) {
+    if (student1.GPA === student2.GPA) return `Both students have the same GPA`;
+
+    const topStudent = student1.GPA > student2.GPA ? student1 : student2;
+
+    return `${topStudent.firstName} ${topStudent.lastName} has the higher GPA.`;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
